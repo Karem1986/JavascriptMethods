7a1edb2989890d7140eced19a7f222eabd160f77
Karem <karem.ortiz@outlook.com>
2021-07-01 10:51:05 +0200

    added include and length array methods

---
+++ arrays/array.includes.js
@@ -0,0 +1,15 @@
+
+
+function checkItems(shoppingCart, shoppingItem) {
+    return shoppingCart.includes(shoppingItem);
+}
+// console.log(checkItems(['boek']))
+// console.log(checkItems(['kitten melk', 'apples']))
+
+//Returns the size of the array: 
+function getArraySize(arr) {
+return arr.length
+    }
+    
+    console.log(getArraySize(["a", "abc"])); 
+    console.log(getArraySize(["Sam", "Alex", "Charlie"])); 
--- arrays/map.js
+++ arrays/map.js
@@ -1,4 +1,4 @@
-//Array function syntax for map:
+//Horses array example
 let horses = [
     {name: 'geof',  height: 15},
     {name: 'gary', height: 14},
@@ -9,8 +9,9 @@ let horses = [
 const names = horses.map(function(i) {
     return i.name
 })
-console.log(names)
-// Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize() 
+// console.log(names)
+//
+//Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize() 
 // function to return a string where every other word is in all caps. 
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take. 
 // ex: swapCase('hey gurl, lets javascript together sometime') // => "HEY gurl, LETS javascript TOGETHER sometime"
@@ -28,13 +29,11 @@ console.log(names)
 // }
 let swapCase = function(string){
   const newString = string.split(' ').map((word, index)=> {
-		// console.log(word, index)
     if(index % 2) { //we make it to return every other word in upper case, as long as the index is remainder of 2(odd index number)
-			// console.log({word})
        return word.toUpperCase()
     }
 		return word
   })
 return newString
 }
-console.log(swapCase('this is fun and lekker'))
+console.log(swapCase('this is fun and i do it often '))
